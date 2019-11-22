c65250dcd9ac28459f54a703561084e9d5eb12ee
Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2019-11-23 00:18:04 +0200

    Add product management structure

---
+++ controllers/ProductController.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const middleware = require("../middleware/AuthMiddleware");
+const productService = require("../services/ProductService");
+
+module.exports = router;
---
+++ models/Product.js
@@ -0,0 +1,41 @@
+
+const mongoose = require("mongoose");
+
+const Product = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    seller: {
+        type: String
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    availableKg: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+}, {
+    collection: 'products',
+    versionKey: false
+});
+
+module.exports = mongoose.model("Product", Product);
