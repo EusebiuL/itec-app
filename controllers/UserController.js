f6176e03b2d06bdeeccd5d519c05ec7265c39253
Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2019-11-22 23:41:37 +0200

    Remove auth middleware for user endpoints

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,9 +4,9 @@ const userService = require("../services/UserService");
 
 router.post("/login", userService.login);
 router.post("/register", userService.register);
-router.get("/users", middleware, userService.getUsers);
-router.get("/buyers", middleware, userService.getBuyers);
-router.get("/sellers", middleware, userService.getSellers);
+router.get("/users", userService.getUsers);
+router.get("/buyers", userService.getBuyers);
+router.get("/sellers", userService.getSellers);
 router.get("/user/:id", userService.getById);
 
 module.exports = router;
