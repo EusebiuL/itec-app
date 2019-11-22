d66c64194b32f49800a2e37feb17a457daea442b
Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2019-11-22 12:23:38 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const router = require("express").Router();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+
+require('./config/passport');
+
+const cors = require('cors');
+
+app.options('*', cors())
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(bodyParser.json());
+
+router.use(require(__dirname + "/controllers/UserController"));
+
+app.use('/api/v1', router);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = require(__dirname + "/app.js");
+const bodyParser = require('body-parser');
+const PORT = (process.argv.length === 3 && Number.parseInt(process.argv[2])) || 4000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./DB.js');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.DB, { useNewUrlParser: true, useCreateIndex: true, }).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the database' + err) }
+);
+  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function () {
+  console.log('Server is running on Port:', PORT);
+});
+
+app.use(async (req, res) => {
+  res.status(404).json({ message: 'Invalid route' });
+})
