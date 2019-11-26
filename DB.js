99e59dd50fd7df662301a24e0754c602159d2793
Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2019-11-26 21:02:39 +0200

    Refactor db connection

--- DB.js
+++ DB.js
@@ -1,3 +1,26 @@
+const mongoose = require('mongoose');
+
+const {
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+  } = process.env;
+
+const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+const options = {
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500, 
+    connectTimeoutMS: 10000,
+  };
+
+mongoose.connect(url, options).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' + err) }
+  );
+
+
 module.exports = {
     DB: 'mongodb://localhost:27017/itec'
 }
